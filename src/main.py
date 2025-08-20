3e46943732d9833c39a88b0ee2e0d22e38e30a5c
soravitsa-sketch <soravit.sa@kkumail.com>
2025-08-20 21:43:57 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Battleship")
+print("ยินดีต้อนรับสู่โปรแกรม Battleship Game")
+print("พิมพ์ 'quit' เพื่อออกจากโปรแกรม")
+
+#ทำงานซ้ำ
+while True:
+    ships_board = [['O'] * 10 for _ in range(10)]
+
+    board_to_print = ships_board
+        
+    print("  " + " ".join("ABCDEFGHIJ"))
+    for i, row in enumerate(board_to_print):
+        print(f"{i+1:<2}" + " ".join(row))
+        
+    command = input("\n> ").lower()
+    
+    if command == 'quit':
+        print("ลาก่อน! ขอให้สนุกกับการฝึกพิมพ์นะครับ/คะ")
+        break
+    else:
+        print("คำสั่งไม่ถูกต้อง กรุณาลองใหม่อีกครั้ง")
